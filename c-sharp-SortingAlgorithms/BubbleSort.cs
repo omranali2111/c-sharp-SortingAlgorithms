c4de7425aceb077efe79df6239f12123e216e0ce
omranali2111 <aljabri1234@hotmail.com>
2023-07-19 10:55:34 +0400

    using BubbleSort to sort list of int from small to larg

---
+++ c-sharp-SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_SortingAlgorithms
+{
+    internal class BubbleSort
+    {
+        public static List<int> BubbleSortFun(List<int> list)
+        {
+            int n = list.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++) // Fix: Change n-1 to n-i-1
+                {
+                    if (list[j] > list[j + 1])
+                    {
+                        // Swap the elements
+                        int temp = list[j];
+                        list[j] = list[j + 1];
+                        list[j + 1] = temp;
+                    }
+                }
+            }
+
+            return list;
+        }
+    }
+
+}
---
+++ c-sharp-SortingAlgorithms/Program.cs
@@ -0,0 +1,20 @@
+﻿using c_sharp_SortingAlgorithms;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> numbers = new List<int> { 5, 2, 9, 1, 3 };
+
+       
+
+        List<int> sortedNumbers = BubbleSort.BubbleSortFun(numbers);
+
+        Console.WriteLine("Sorted List:");
+        foreach (int num in sortedNumbers)
+        {
+            Console.Write(num + " ");
+        }
+       
+    }
+}
